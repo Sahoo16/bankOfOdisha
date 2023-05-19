4c8d532bb3ed1aa19ff6ee6b92170ec51714720c
Sahoo16 <122251450+Sahoo16@users.noreply.github.com>
2023-05-19 12:23:24 +0530

    uplode file

---
+++ about.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- external css bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    
+    <!-- <link href="./css/style.css" rel="text/stylesheet"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to </title>
+</head>
+<body>
+    <!-- header with connection and sql code -->
+  <?php require_once './include/header.php' ?>
+  <h1 class="display-1 pt-4 mt-sm-3">About Bank Of Odisha</h1>
+
+  <blockquote class="blockquote text-center">
+  <p class="mb-0">Bank of Odisha is a leading financial institution dedicated to providing exceptional banking services to the people of Odisha, a vibrant state in eastern India. Established in 1965, the bank has been a trusted partner for individuals, businesses, and communities, offering a wide range of banking products and services to cater to their diverse financial needs.
+
+As a customer-centric bank, Bank of Odisha is committed to fostering financial inclusion and empowering the people of Odisha to achieve their dreams. The bank's mission is to be a catalyst for economic growth and development in the state by delivering innovative and accessible banking solutions.
+
+Bank of Odisha takes pride in its strong network of branches strategically located across the state, ensuring convenient access to banking facilities for customers from urban centers to rural areas. With a team of dedicated professionals and a customer-first approach, the bank strives to provide personalized attention, excellent customer service, and tailor-made financial solutions to meet the unique requirements of each customer.</p>
+  <footer class="blockquote-footer">famous in <cite title="Source Title">Reliability</cite></footer>
+</blockquote>
+
+
+ <!-- footer  -->
+ <?php require_once './include/footer.php' ?>
+    
+</body>
+</html>
